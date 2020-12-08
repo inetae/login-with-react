2cd21c8bbc5714b18d58da69f6f6343e3e553a1a
Ineta Eidininkaite <i.eidininkaite@spectrofinance.lt>
2020-12-09 00:04:19 +0200

    set loading

--- src/components/Servers/Servers.tsx
+++ src/components/Servers/Servers.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { Redirect } from '@reach/router'
 import Table from 'components/Table/Table';
@@ -7,10 +7,12 @@ import { Context } from 'controllers/Store';
 import style from './Servers.module.scss';
 
 const Servers = () => {
+    const [isLoading, setIsLoading] = useState(false);
     // @ts-ignore
     const [state, dispatch] = useContext(Context);
 
     const getServers = () => {
+        setIsLoading(true);
         const config = {
             headers: { Authorization: `Bearer ${getCookie('access_token')}` }
         };
@@ -20,17 +22,17 @@ const Servers = () => {
                 if (response.status !== 200) dispatch({ type: 'SET_ERROR', payload: 'Something went wrong...'});
 
                 if (response.status === 200) {
-
                     setTimeout(() => {
                         dispatch({ type: 'SET_DATA', payload: response.data});
+                        setIsLoading(false);
                     }, 2500);
                 }
             })
             .catch((error) => {
-
                 setTimeout(() => {
                     setCookie('access_token', '', 0);
                     dispatch({ type: 'SET_ERROR', payload: error});
+                    setIsLoading(false);
                 }, 2500);
             });
     };
@@ -39,7 +41,7 @@ const Servers = () => {
         getServers();
     }, []);
 
-    if (state.data.length === 0) return <div className={style.svgWrapper}>
+    if (state.data.length === 0 && isLoading) return <div className={style.svgWrapper}>
         <svg viewBox="0 0 360 352" fill="none" xmlns="http://www.w3.org/2000/svg" className={style.svg}>
             <g clipPath="url(#clip0)">
                 <path className={style.path} d="M33.53 310.521C12.43 281.328 0 245.486 0 206.753 0 109.055 79.055 30 176.464 30c97.409 0 176.464 79.199 176.464 176.753 0 38.733-12.429 74.575-33.53 103.768l-84.835-137.876-8.238 13.875 8.238 38.443-58.244-99.866-35.986 60.989 8.382 38.877-30.494-52.318L33.53 310.521z"/>
